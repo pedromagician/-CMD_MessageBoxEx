eb622c8007c9a5a85af9a1b46aab9d624dc9acf4
Pedro <pedromagician@gmail.com>
2023-11-25 19:21:57 +0100

    update: MLD

--- stdafx.h
+++ stdafx.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <windows.h>
-#include <string>
 #include <algorithm>
 #include <atlbase.h>
-#include <activscp.h>
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
 #include <io.h>
 #include <fcntl.h>
+
+#include "Debug.h"
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
 
 using namespace std;
